aee67e39953a32969c5cd2a996ecba8957ce3291
Devesh Gupta <guptagovind0370@gmail.com>
2026-08-21 02:10:56 +0530

    Time: 3 ms (93.99%), Space: 44.4 MB (74.9%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char ch[]=new char[26];
+        int l1=s.length();
+        int l2=t.length();
+        if(l1!=l2)return false;
+        for(int i=0;i<l1;i++)
+        {
+            ch[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<l2;i++)
+        {
+            ch[t.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(ch[i]!=0)
+            return false;
+        }
+        return true;
+    }
+}
