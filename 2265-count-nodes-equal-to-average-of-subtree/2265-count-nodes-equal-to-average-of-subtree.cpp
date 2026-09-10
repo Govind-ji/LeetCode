2c9c1fd3627d1f0019e89bfcdf60b68bf658cbab
Devesh Gupta <guptagovind0370@gmail.com>
2026-09-10 11:45:38 +0530

    Time: 5 ms (72.11%), Space: 15.6 MB (97.32%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -1,25 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int ans = 0;
+pair <int,int> cnt(TreeNode * root,int & ans)
+{
+    if(!root)return {0,0};
+    auto l=cnt(root->left,ans);
+    auto r=cnt(root->right,ans);
+    int sum=l.first+r.first+root->val;
+    int c=l.second+r.second+1;
+    if(sum/c==root->val)
+    ans++;
+    return {sum,c};
 
-    pair<int,int> postorder(TreeNode* root) {
-        if (!root) return {0, 0};
-
-        auto left = postorder(root->left);
-        auto right = postorder(root->right);
-
-        int sum = left.first + right.first + root->val;
-        int cnt = left.second + right.second + 1;
-
-        int avg = sum / cnt;
-
-        if (avg == root->val) ans++;
-
-        return {sum, cnt};
-    }
-
+}
     int averageOfSubtree(TreeNode* root) {
-        postorder(root);
+        int ans=0;
+        cnt(root,ans);
         return ans;
     }
 };
